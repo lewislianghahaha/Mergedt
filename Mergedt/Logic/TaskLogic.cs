2916f20fd0d717691e1ecfa6a3f3834a8e5d0874
Lewis <heshanpin2016@163.com>
2019-06-27 17:37:58 +0800

    20190627:初始添加

--- Mergedt/Logic/TaskLogic.cs
+++ Mergedt/Logic/TaskLogic.cs
@@ -1,13 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Mergedt.Logic;
 
 namespace MergeDt.Logic
 {
     public class TaskLogic
     {
+        Import import=new Import();
+        Generate generate=new Generate();
+        Export export=new Export();
+
+        private int _taskid;
+        private string _fileAddress;        //文件地址
+        private DataTable _resultTable;     //记录集DATATABLE（从EXCEL导入的DataTable）
+
+
+
+        #region Set
+
+            /// <summary>
+            /// 中转ID
+            /// </summary>
+            public int TaskId { set { _taskid = value; } }
+
+            /// <summary>
+            /// //接收文件地址信息
+            /// </summary>
+            public string FileAddress { set { _fileAddress = value; } }
+
+        #endregion
+
+        #region Get
+            /// <summary>
+            ///返回DataTable至主窗体
+            /// </summary>
+            public DataTable RestulTable => _resultTable;
+
+            
+
+        #endregion
+
+
+        public void StartTask()
+        {
+            Thread.Sleep(1000);
+
+            switch (_taskid)
+            {
+                //打开EXCEL并导入至DataTable功能
+                case 1:
+                    OpenExcelImporttoDt(_fileAddress);
+                    break;
+                //运算
+                case 2:
+                    GenerateRecord();
+                    break;
+                //导出
+                case 3:
+                    ExportDtToExcel();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 打开EXCEL并导入至DataTable功能
+        /// </summary>
+        private void OpenExcelImporttoDt(string fileAddress)
+        {
+            
+        }
+
+        /// <summary>
+        /// 运算
+        /// </summary>
+        private void GenerateRecord()
+        {
+            
+        }
+
+        /// <summary>
+        /// 导出
+        /// </summary>
+        private void ExportDtToExcel()
+        {
+            
+        }
 
     }
 }
--- Mergedt/Main.cs
+++ Mergedt/Main.cs
@@ -4,22 +4,134 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MergeDt.Logic;
 
 namespace Mergedt
 {
     public partial class Main : Form
     {
+        TaskLogic task=new TaskLogic();
+        Load load = new Load();
+
+        //存放从EXCEL导入的DT
+        private DataTable _exceldt;
+
         public Main()
         {
             InitializeComponent();
+            OnRegisterEvents();
         }
 
         private void OnRegisterEvents()
         {
-            
+            btnimport.Click += Btnimport_Click;
+            btnGenerate.Click += BtnGenerate_Click;
+            btnExport.Click += BtnExport_Click;
+            tmclose.Click += Tmclose_Click;
+        }
+
+        /// <summary>
+        /// 打开EXCEL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btnimport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var openFileDialog = new OpenFileDialog { Filter = "Xlsx文件|*.xlsx" };
+                if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+                var fileAdd = openFileDialog.FileName;
+
+                //将所需的值赋到Task类内
+                task.TaskId = 1;
+                task.FileAddress = fileAdd;
+
+
+                //使用子线程工作(作用:通过调用子线程进行控制Load窗体的关闭情况)
+                new Thread(Start).Start();
+                load.StartPosition = FormStartPosition.CenterScreen;
+                load.ShowDialog();
+
+                if (task.RestulTable.Rows.Count == 0) throw new Exception("不能成功导入EXCEL内容,请检查模板是否正确.");
+                else
+                {
+                    MessageBox.Show($"导入成功,请按'运算'按钮继续", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 运算
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 导出EXCEL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Tmclose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        ///子线程使用(重:用于监视功能调用情况,当完成时进行关闭LoadForm)
+        /// </summary>
+        private void Start()
+        {
+            task.StartTask();
+
+            //当完成后将Form2子窗体关闭
+            this.Invoke((ThreadStart)(() => {
+                load.Close();
+            }));
         }
 
     }
